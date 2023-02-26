957a5b955b371403c258e26d3d92b04063f69164
letien23 <letien27012002@gmail.com>
2023-02-26 20:15:36 +0700

    update

--- src/screens/ProductUser/ProductInfo.js
+++ src/screens/ProductUser/ProductInfo.js
@@ -1,23 +1,35 @@
 import React from "react";
 import EditProduct from "./EditProduct";
+import { useState } from "react";
+import ButtonSubmit from "../../components/ButtonSubmit";
 
 
-export default function ProductInfo({ data, onDelete }) {
+
+export default function ProductInfo({ data, onDelete, closeModal,setEditData}) {
+  const handlerEditData = (e) => {
+    
+    closeModal(true);
+    setEditData(data);
+};
   return (
-    <div className="product-info">
-      <div>{data.id}</div>
-      <div>{data.pro_name}</div>
-      <div><img className="product-info-img" src="/image/avata.png" alt="" /></div>
-      <div>{data.type}</div>
-      <div>{data.price}</div>
-      <div>{data.description}</div>
-      <div>{data.quantity}</div>
-      <div className="product-info__action">
-        <EditProduct data={data} />
-        <button onClick={() => onDelete(data.id)} className="product-info__action">
-          <img className="product-info__icon" src="/image/Delete.png" alt="" />
-        </button>
-      </div>
-    </div>
+    <>
+      <div className="product-info">
+        <div>{data.id}</div>
+        <div>{data.name}</div>
+        <div><img className="product-info-img" src="/image/avata.png" alt="" /></div>
+        <div>{data.type}</div>
+        <div>{data.price}</div>
+        <div>{data.description}</div>
+        <div>{data.quantity}</div>
+        <div className="product-info__action">
+          <button  className="product-info__action" onClick={handlerEditData}>
+            <img className="product-info__icon" src="/image/pencil.png" alt="" />
+          </button>
+          <button className="product-info__action" onClick={() => onDelete(data.id)} >
+            <img className="product-info__icon" src="/image/Delete.png" alt="" />
+          </button>
+        </div>
+      </div></>
+
   );
 }
--- src/screens/ProductUser/ProductUser.js
+++ src/screens/ProductUser/ProductUser.js
@@ -2,11 +2,20 @@ import { React, useEffect, useState } from "react";
 import axios from "axios";
 import AddProduct from "./AddProduct";
 import ProductInfo from "./ProductInfo";
+import FormInput from "../../components/FormInput";
 import HeaderShopOwner from "../../components/HeaderShopOwner";
+import ButtonSubmit from "../../components/ButtonSubmit";
+import EditProduct from "./EditProduct";
 
 const Product = () => {
     const [product, setProduct] = useState([]);
-    const [toggle, setToggle] = useState(false)
+    const [toggle, setToggle] = useState(false);
+    const [modalAddProduct, setModalAddProduct] = useState(false);
+    const [modalEditProduct, setModalEditProduct] = useState(false);
+    const [editData, setEditData] = useState([]);
+
+
+
 
     useEffect(() => {
         axios
@@ -14,7 +23,7 @@ const Product = () => {
             .then((res) => {
                 setProduct(res.data);
             })
-            .catch(err=>{
+            .catch(err => {
                 console.log("Err: ", err)
             })
     }, [toggle]);
@@ -30,62 +39,82 @@ const Product = () => {
                 })
         }
     }
+    const handlerInput = (e) => {
+        const { name, value } = e.target;
+        console.log(product);
+        setProduct({
+            ...product,
+            [name]: value,
+        });
+    };
+
     return (
-        <div className="product-user">
-            <div className="product-user__header"><HeaderShopOwner/></div>
-            <hr></hr>
-            <div className="product-user__body">
+        <>
+            {modalAddProduct && <AddProduct closeModal={setModalAddProduct} />}
+            {modalEditProduct && <EditProduct data={editData} closeModal={setModalEditProduct} />}
+            <div className="product-user">
+                <div className="product-user__header"><HeaderShopOwner /></div>
+                <hr></hr>
+                <div className="product-user__body">
+                    <div className="product-user__body__left-menu">
+                        <div className="product-user__body__left-menu__avata">
+                            <img className="product-user-img" src="/image/avata.png" alt="" />
+                            <h3>Your market place</h3>
+                            <p>Ngô Thị Tròn</p>
+                        </div>
+                        <div className="product-user__body__left-menu__menu">
+                            <ul>
+                                <li><img className="product-user-icon" src="/image/Yourpro.png" alt="" /> Your product</li>
+                                <li><img className="product-user-icon" src="/image/manacus.png" alt="" /> Manage customers</li>
+                                <li><img className="product-user-icon" src="/image/manare.png" alt="" /> Manage revenue</li>
+                                <li><img className="product-user-icon" src="/image/manaor.png" alt="" /> Manage order</li>
+                                <li><img className="product-user-icon" src="/image/home.png" alt="" /> Home</li>
+                                <li><img className="product-user-icon" src="/image/Logout.png" alt="" /> Log out</li>
+                            </ul>
 
-                <div className="product-user__body__left-menu">
-                    <div className="product-user__body__left-menu__avata">
-                        <img className="product-user-img" src="/image/avata.png" alt="" />
-                        <h3>Your market place</h3>
-                        <p>Ngô Thị Tròn</p>
+                        </div>
                     </div>
-                    <div className="product-user__body__left-menu__menu">
-                        <ul>
-                            <li><img className="product-user-icon" src="/image/Yourpro.png" alt="" /> Your product</li>
-                            <li><img className="product-user-icon" src="/image/manacus.png" alt="" /> Manage customers</li>
-                            <li><img className="product-user-icon" src="/image/manare.png" alt="" /> Manage revenue</li>
-                            <li><img className="product-user-icon" src="/image/manaor.png" alt="" /> Manage order</li>
-                            <li><img className="product-user-icon" src="/image/home.png" alt="" /> Home</li>
-                            <li><img className="product-user-icon" src="/image/Logout.png" alt="" /> Log out</li>
-                        </ul>
+                    <div className="product-user__body__right-menu">
+                        <div className="right-menu">
+                            <h1>Your products</h1>
 
-                    </div>
-                </div>
-                <div className="product-user__body__right-menu">
-                    <h1>Your products</h1>
+                            <div className="product-user__body__right-menu__head">
+                                <input name="filter"
+                                    placeholder="Filter"
+                                    onChange={handlerInput}
+                                    type="text"
+                                    className="product-user__body__right-menu__head__filter"
+                                />
+                                <ButtonSubmit className="product-user__body__right-menu__head__add-button" title="Add product" onClick={() => setModalAddProduct(true)} />
+                            </div>
+                            <div className="product-user__body__right-menu__body">
+                                <div>ID</div>
+                                <div>Name</div>
+                                <div>Image</div>
+                                <div>Type</div>
+                                <div>Price</div>
+                                <div>Description</div>
+                                <div>Quantity</div>
+                                <div>Action</div>
+                            </div>
+                            <hr />
+                            <div className="product-user__body__right-menu__list">
+                                {product.map((product) => (
+                                    <div>
+                                        <ProductInfo key={product.id} data={product} onDelete={deleteHandle} closeModal={setModalEditProduct} setEditData={setEditData}/>
+                                        <hr />
+                                    </div>
 
-                    <div className="product-user__body__right-menu__head">
-                        <input placeholder="Filter"></input>
-                        <AddProduct />
-                    </div>
-                    <div className="product-user__body__right-menu__body">
-                        <div>ID</div>
-                        <div>Name</div>
-                        <div>Image</div>
-                        <div>Type</div>
-                        <div>Price</div>
-                        <div>Description</div>
-                        <div>Quantity</div>
-                        <div>Action</div>
-                    </div>
-                    <hr/>
-                    <div className="product-user__body__right-menu__list">
-                        {product.map((product) => (
-                            <div>
-                                <ProductInfo key={product.id} data={product} onDelete={deleteHandle} />
-                                <hr/>
+                                ))}
                             </div>
 
-                        ))}
+                        </div>
+
                     </div>
-
                 </div>
-            </div>
 
-        </div>
+            </div></>
+
     )
 };
 export default Product;

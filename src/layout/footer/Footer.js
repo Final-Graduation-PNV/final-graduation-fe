a7a8a06f0bc007b88e63869fdcf11fc07dc08c17
letien23 <letien27012002@gmail.com>
2023-03-16 08:35:58 +0700

    update footer

---
+++ src/layout/footer/Footer.js
@@ -0,0 +1,65 @@
+import React from "react";
+function Footer() {
+    return (
+        <>
+            <div className="footer">
+                <div className="footer__menu">
+                    <div className="footer__menu__title">
+                        <h2>RESOURCES</h2>
+                    </div>
+                    <div className="footer__menu__contents">
+                        <ul>
+                            <li>Application</li>
+                            <li>Documentation</li>
+                            <li>Systems</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer__menu">
+                    <div className="footer__menu__title">
+                        <h2>PRICING</h2>
+                    </div>
+                    <div className="footer__menu__contents">
+                        <ul>
+                            <li>Overview</li>
+                            <li>Premium Plans</li>
+                            <li>Affiliate Program</li>
+                            <li>Promotions</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="footer__menu">
+                    <div className="footer__menu__title">
+                        <h2>COMPANY</h2>
+                    </div>
+                    <div className="footer__menu__contents">
+                        <ul>
+                            <li>About Us</li>
+                            <li>Blog</li>
+                            <li>Partnerships</li>
+                            <li>Careers</li>
+                            <li>Press</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="footer__menu">
+                    <div className="footer__menu__title">
+                        <h2>SOCIAL</h2>
+                    </div>
+                    <div className="footer__menu__contents">
+                        <ul>
+                            <li>Facebook</li>
+                            <li>Instagram</li>
+                            <li>LinkedIn</li>
+                            <li>Twitter</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Footer;

2df59358c7a45f5c5e476c8a0adb68f53caf06a7
Ngo Thi Tron <ngotron01.06@gmail.com>
2023-03-16 21:27:12 +0700

    finshed homepage footer and fix some bugs home page and shop onwer

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,65 +1,44 @@
+import { faFacebookF, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import cashpayment from "../../assets/Image/cashpayment.png";
+import logo from "../../assets/Image/logo.png";
+import vnpay from "../../assets/Image/vnpay.png";
 const Footer = () => {
     return (
-        <>
-            <div className="footer">
-                <div className="footer__menu">
-                    <div className="footer__menu__title">
-                        <h2>RESOURCES</h2>
-                    </div>
-                    <div className="footer__menu__contents">
-                        <ul>
-                            <li>Application</li>
-                            <li>Documentation</li>
-                            <li>Systems</li>
-                            <li>FAQ</li>
-                        </ul>
+        <div className="footer">
+            <div className="footer-top">
+                <div className="footer-logo">
+                    <img src={logo} />
+                </div>
+                <div className="footer-idea">
+                    <p>Contribute ideas</p>
+                    <div className="footer-idea-contributes">
+                        <input type="text" placeholder="Please enter your ideas" />
+                        <button>Email</button>
                     </div>
                 </div>
-                <div className="footer__menu">
-                    <div className="footer__menu__title">
-                        <h2>PRICING</h2>
-                    </div>
-                    <div className="footer__menu__contents">
-                        <ul>
-                            <li>Overview</li>
-                            <li>Premium Plans</li>
-                            <li>Affiliate Program</li>
-                            <li>Promotions</li>
-                        </ul>
-                    </div>
-
-                </div>
-                <div className="footer__menu">
-                    <div className="footer__menu__title">
-                        <h2>COMPANY</h2>
-                    </div>
-                    <div className="footer__menu__contents">
-                        <ul>
-                            <li>About Us</li>
-                            <li>Blog</li>
-                            <li>Partnerships</li>
-                            <li>Careers</li>
-                            <li>Press</li>
-                        </ul>
-                    </div>
-
-                </div>
-                <div className="footer__menu">
-                    <div className="footer__menu__title">
-                        <h2>SOCIAL</h2>
-                    </div>
-                    <div className="footer__menu__contents">
-                        <ul>
-                            <li>Facebook</li>
-                            <li>Instagram</li>
-                            <li>LinkedIn</li>
-                            <li>Twitter</li>
-                        </ul>
+                <div className="footer-ordering">
+                    <p>Payment methods</p>
+                    <div className="footer-idea-ordering">
+                        <img src={cashpayment} />
+                        <img src={vnpay} />
                     </div>
                 </div>
-            </div>
-        </>
+                <div className="footer-Contact">
+                    <p>Contact with us</p>
+                    <div className="footer-idea-contact">
+                        <a href="https://www.facebook.com/"><FontAwesomeIcon className="icon-footer__faPhone" icon={faPhone} /></a>
+                        <a href="https://www.facebook.com/"><FontAwesomeIcon className="icon-footer__faEnvelope" icon={faEnvelope} /></a>
+                        <a href="https://www.facebook.com/"><FontAwesomeIcon className="icon-footer__faTwitter" icon={faTwitter} /></a>
+                        <a href="https://www.facebook.com/"><FontAwesomeIcon className="icon-footer__faInstagram" icon={faInstagram} /></a>
+                        <a href="https://www.facebook.com/"><FontAwesomeIcon className="icon-footer__faFacebookF" icon={faFacebookF} /></a>
+                    </div >
+                </div >
+            </div >
+            <div className="footer-more">About Us | Sitemap | Contact Us | FAQS | Blog | Offers | Terms & Conditions | Customize Your Bouquet | Become a Partner</div>
+        </div >
     )
 }
 export default Footer;

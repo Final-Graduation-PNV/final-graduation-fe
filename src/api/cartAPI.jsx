a06397013b887cacf8480a1d42d241fc11c19491
Ngo Thi Tron <ngotron01.06@gmail.com>
2023-03-22 09:52:52 +0700

    update shop onwer

--- src/api/cartAPI.jsx
+++ src/api/cartAPI.jsx
@@ -1,24 +1,25 @@
 import http from "./http";
 
 const CART_API_URL = "user/carts";
-const CLEAR_ALL_CART_URL = 'user/clear-carts';
+const ADDTOCART_API_URL = "/user/products";
+const CLEAR_ALL_CART_URL = "user/clear-carts";
 
 export const getCarts = async () => {
-  return await http.get(CART_API_URL)
-}
+  return await http.get(CART_API_URL);
+};
 
 export const addToCart = async (id, quantity) => {
-  return await http.post(`${CART_API_URL}/${id}`, { quantity })
-}
+  return await http.post(`${ADDTOCART_API_URL}/${id}/cart`, { quantity });
+};
 
 export const deleteCartById = async (id) => {
-  return await http.delete(`${CART_API_URL}/${id}`)
-}
+  return await http.delete(`${CART_API_URL}/${id}`);
+};
 
 export const deleteAllCart = async () => {
-  return await http.delete(CLEAR_ALL_CART_URL)
-}
+  return await http.delete(CLEAR_ALL_CART_URL);
+};
 
 export const updateCart = async (id, quantity) => {
-  return await http.patch(`${CART_API_URL}/${id}`, { quantity: quantity })
-}
+  return await http.patch(`${CART_API_URL}/${id}`, { quantity: quantity });
+};
--- src/pages/Modals/InforMarketPL.js
+++ src/pages/Modals/InforMarketPL.js
@@ -12,52 +12,51 @@ Geocode.setApiKey("AIzaSyBnz2gvBIM3N89kMVI4YJT8oOGZT31NACU");
 
 function InforMarketPL({ closeModal }) {
 
-  const user_id = localStorage.getItem("user_id");
   const token = localStorage.getItem("token")
   const UrlShopOnwer = "http://ec2-54-67-114-9.us-west-1.compute.amazonaws.com/api/user/be-shop";
   const navigate = useNavigate();
 
+  const [name, setName] = useState("")
   const [birth, setBirth] = useState()
   const [phone, setPhone] = useState("")
-  const [gender, setGender] = useState("")
-  const [city, setCity] = useState("")
+  const [gender, setGender] = useState("Male")
+  const [city, setCity] = useState("Hà Nội")
   const [address, setAddress] = useState("")
-  const [latitude, setLatitude] = useState("")
-  const [longitude, setLongitude] = useState("")
+  const [latitude, setLatitude] = useState(null)
+  const [longitude, setLongitude] = useState(null)
 
   const initialError = Object.freeze({
+    name: "",
     phone: "",
     birth: "",
     gender: "",
     address: "",
     citiesL: "",
-    latitude: "",
-    longitude: ""
-
   })
 
-  const handleSubmit = (add) => {
-    // event.preventDefault();
-    Geocode.fromAddress(add).then(
-      (response) => {
-        const { lat, lng } = response.results[0].geometry.location;
-        setLatitude(lat);
-        setLongitude(lng);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  };
-  console.log("địa chỉ nha:", latitude, longitude)
+  console.log("Name: ", name, " phone: ", phone, "birth: ", birth, "gender: ", gender, "Address: ", address, "city: ", city, "longitude: ", longitude, "latitude: ", latitude)
+  // const handleSubmit = (add) => {
+  //   // event.preventDefault();
+  //   Geocode.fromAddress(add).then(
+  //     (response) => {
+  //       const { lat, lng } = response.results[0].geometry.location;
+  //       setLatitude(lat);
+  //       setLongitude(lng);
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     }
+  //   );
+  // };
+
   const [error, setError] = useState(initialError)
 
   const shopOnwerhanler = () => {
     resetErrors()
-    handleSubmit(address);
+    // handleSubmit(address);
     axios.post(UrlShopOnwer,
       {
-        user_id,
+        name,
         phone,
         birth,
         gender,
@@ -98,11 +97,17 @@ function InforMarketPL({ closeModal }) {
           </div>
           <div className="infor-text">
             <p className="infor-text__tittle">Delivery Address</p>
+            <div className="infor-name">
+              <p className="infor-text__phone" >Name:</p>
+              <input className="infor-nameshop__input" type="text" placeholder="Please your name" value={name} onChange={(e) => { setName(e.target.value) }} />
+              {error.name && <span style={{ color: 'red', fontSize: 12 }}>{error.name}</span>}
+            </div>
             <div className="info-person">
+
               <div className="infor-name">
                 <p className="infor-text__phone" >Phone:</p>
                 <input className="infor-phone__input" type="text" placeholder="Please your phone" value={phone} onChange={(e) => { setPhone(e.target.value) }} />
-                {error.phone && <span style={{ color: 'red', fontSize: 12 }}>{error.phone}</span>}
+                {error.phone && <span style={{ color: 'red', fontSize: 12, marginBottom: -14 }}>{error.phone}</span>}
               </div>
               <div className="infor-gender">
                 <p className="infor-text__gender" >Gender:</p>
@@ -125,7 +130,7 @@ function InforMarketPL({ closeModal }) {
                   <option key={id} value={city.name}>{city.name}</option>
                 ))}
               </select>
-              {error.city && <span style={{ color: 'red', fontSize: 12 }}>{error.city}</span>}
+              {error.citiesL && <span style={{ color: 'red', fontSize: 12 }}>{error.city}</span>}
             </div>
             <div className="infor-despayment">
               <p>Specific address:</p>
